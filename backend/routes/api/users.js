86461460b643054f6baa59f7bda6d6a9de405589
mroberts1137 <mroberts1137@gmail.com>
2024-04-08 22:33:56 -0400

    add user registration validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 
 // @route GET api/users/test
@@ -12,29 +14,45 @@ router.get('/test', (req, res) => {
 // @route POST api/users/register
 // @desc Register a new user
 // @access Public
-router.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  console.log(req.body);
-  // Check if the email already exists
-  User.findOne({ email: email })
-    .then((user) => {
-      if (user) {
-        return res.status(400).json({ email: 'Email already exists' });
-      } else {
-        // Create new user
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password
-        });
-        // Save the new user
-        newUser
-          .save()
-          .then((user) => res.json(user))
-          .catch((err) => console.log(err));
-      }
-    })
-    .catch((err) => console.log(err));
-});
+router.post(
+  '/register',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    console.log(req.body);
+    // Check if the email already exists
+    User.findOne({ email: email })
+      .then((user) => {
+        if (user) {
+          return res.status(400).json({ email: 'Email already exists' });
+        } else {
+          // Create new user
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+          });
+          // Save the new user
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+);
 
 module.exports = router;
